6a6a3b4265b7b90aeea26db088824eb726c94490
Dayana KEO <keo.dayana@hotmail.fr>
2022-03-02 17:11:18 +0100

    Ajout des liens de page inscriptions, login, temoignage..

--- includes/nav.php
+++ includes/nav.php
@@ -2,6 +2,20 @@
     <nav role="navigation">
         <ul>
             <li><a href="index.php?page=home">Accueil</a></li>
+            <li><a href="index.php?page=inscription">Inscription</a></li>
+            <li><a href="index.php?page=temoignage">Témoignage</a></li>
+
+            <?php
+                if(isset($_SESSION['login']) && $_SESSION['login'] === true) {
+                    echo "<li><a href=\"index.php?page=logout\">Logout</a></li>";
+                    
+                }
+                else {
+                    echo "<li><a href=\"index.php?page=login\">Login</a></li>";
+                    
+                }
+            ?>
+
         </ul>
     </nav>
 </header>
